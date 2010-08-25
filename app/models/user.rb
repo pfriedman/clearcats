26176d39f8d29ecbd6dfeed65eaf4427e4888c5a
Paul Friedman <p-friedman@northwestern.edu>
2010-08-25 14:35:54 -0500

    updated annotation - organizational_unit_id added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,24 @@
 #
 # Table name: users
 #
-#  id             :integer         not null, primary key
-#  first_name     :string(255)
-#  middle_name    :string(255)
-#  last_name      :string(255)
-#  title          :string(255)
-#  business_phone :string(255)
-#  fax            :string(255)
-#  email          :string(255)
-#  username       :string(255)
-#  nu_employeeid  :string(255)
-#  personnelid    :string(255)
-#  address        :string(255)
-#  city           :string(255)
-#  state          :string(255)
-#  country        :string(255)
-#  created_at     :datetime
-#  updated_at     :datetime
+#  id                     :integer         not null, primary key
+#  first_name             :string(255)
+#  middle_name            :string(255)
+#  last_name              :string(255)
+#  title                  :string(255)
+#  business_phone         :string(255)
+#  fax                    :string(255)
+#  email                  :string(255)
+#  username               :string(255)
+#  nu_employeeid          :string(255)
+#  personnelid            :string(255)
+#  address                :string(255)
+#  city                   :string(255)
+#  state                  :string(255)
+#  country                :string(255)
+#  organizational_unit_id :integer
+#  created_at             :datetime
+#  updated_at             :datetime
 #
 
 class User < ActiveRecord::Base
