3c59a37ccec25c3f27704cc5ea95a633728fa688
Paul Friedman <p-friedman@northwestern.edu>
2010-10-05 10:21:53 -0500

    changes to welcome index view

--- spec/views/admin/index.html.haml_spec.rb
+++ spec/views/admin/index.html.haml_spec.rb
@@ -12,23 +12,25 @@ describe "/admin/index.html.haml" do
 
       response.should have_tag("h1", "Welcome to ClearCATS!")
 
-      response.should have_tag("h3", "Administrative Actions")
+      response.should have_tag("h3", "Center Manager Actions")
       response.should have_tag("ul#administrative_actions")
       
       response.should have_tag("ul#administrative_actions>li:nth-child(1)>a", "Manage Users")
       response.should have_tag("ul#administrative_actions>li:nth-child(1)>a[href=/users]")
+
+      response.should have_tag("ul#administrative_actions>li:nth-child(2)>a", "Upload CTSA Data")
+      response.should have_tag("ul#administrative_actions>li:nth-child(2)>a[href=/admin/upload_ctsa_data]")
+
+      response.should have_tag("ul#organizational_unit_administrator_actions")
       
-      response.should have_tag("ul#administrative_actions>li:nth-child(2)>a", "Manage Organizational Units")
-      response.should have_tag("ul#administrative_actions>li:nth-child(2)>a[href=/organizational_units]")
+      response.should have_tag("ul#organizational_unit_administrator_actions>li:nth-child(1)>a", "Manage Organizational Units")
+      response.should have_tag("ul#organizational_unit_administrator_actions>li:nth-child(1)>a[href=/organizational_units]")
       
-      response.should have_tag("ul#administrative_actions>li:nth-child(3)>a", "Manage Service Lines")
-      response.should have_tag("ul#administrative_actions>li:nth-child(3)>a[href=/service_lines]")
+      response.should have_tag("ul#organizational_unit_administrator_actions>li:nth-child(2)>a", "Manage Service Lines")
+      response.should have_tag("ul#organizational_unit_administrator_actions>li:nth-child(2)>a[href=/service_lines]")
       
-      response.should have_tag("ul#administrative_actions>li:nth-child(4)>a", "Upload CTSA Data")
-      response.should have_tag("ul#administrative_actions>li:nth-child(4)>a[href=/admin/upload_ctsa_data]")
-      
-      response.should have_tag("ul#administrative_actions>li:nth-child(5)>a", "View CTSA Data")
-      response.should have_tag("ul#administrative_actions>li:nth-child(5)>a[href=/admin/view_ctsa_data]")
+      response.should have_tag("ul#organizational_unit_administrator_actions>li:nth-child(3)>a", "View CTSA Data")
+      response.should have_tag("ul#organizational_unit_administrator_actions>li:nth-child(3)>a[href=/admin/view_ctsa_data]")
     end
     
   end
