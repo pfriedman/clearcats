17ae91a83aeef98ff6b72e12e3852b914c9ef3b4
Paul Friedman <p-friedman@northwestern.edu>
2010-10-26 09:28:12 -0500

    user setup

--- db/user-setup/institutional_affiliates.sql
+++ db/user-setup/institutional_affiliates.sql
@@ -1,4 +1,10 @@
+--- Already in database AFFILIATE_ID 555
+--- NUCATS
+--- Clinical and Translational Sciences Institute
+
+
 --- NUBIC
+--- Biomedical Informatics Center
 
 SELECT * FROM T_INSTITUTIONAL_AFFILIATES WHERE PARENT_ID = 555
 SELECT AFFILIATE_ID FROM T_INSTITUTIONAL_AFFILIATES WHERE NAME_ABBREV = 'CECD'
@@ -38,6 +44,7 @@ VALUES
 )
 
 --- CECD
+--- Center for Education and Career Development
 
 INSERT INTO T_INSTITUTIONAL_AFFILIATES
 (
@@ -66,6 +73,260 @@ VALUES
 , 'Center for Education and Career Development'
 , 'Medical Sch./NUCATS_CECD'
 , 'CECD'
+, 'Active'
+, 61823
+, '192.168.1.221'
+,  sysdate
+)
+
+
+--- CRC
+--- Center for Clinical Research
+
+INSERT INTO T_INSTITUTIONAL_AFFILIATES
+(
+  AFFILIATE_ID
+, PARENT_ID
+, INSTITUTION_ID
+, PRU_ID
+, PRU_PARENT_ID
+, PRU_CAMPUS
+, NAME_FULL
+, NAME_SORT
+, NAME_ABBREV
+, PRU_STATUS
+, ENTERED_ID
+, ENTERED_IP
+, ENTERED_DATE
+)
+VALUES
+(
+  5003
+, 555
+, 1008
+, 5003
+, 555
+, 'Chicago'
+, 'Center for Clinical Research'
+, 'Medical Sch./NUCATS_CRC'
+, 'CRC'
+, 'Active'
+, 61823
+, '192.168.1.221'
+,  sysdate
+)
+
+--- RSP
+--- Regulatory Support Program
+
+INSERT INTO T_INSTITUTIONAL_AFFILIATES
+(
+  AFFILIATE_ID
+, PARENT_ID
+, INSTITUTION_ID
+, PRU_ID
+, PRU_PARENT_ID
+, PRU_CAMPUS
+, NAME_FULL
+, NAME_SORT
+, NAME_ABBREV
+, PRU_STATUS
+, ENTERED_ID
+, ENTERED_IP
+, ENTERED_DATE
+)
+VALUES
+(
+  5004
+, 5003
+, 1008
+, 5004
+, 5003
+, 'Chicago'
+, 'Regulatory Support Program'
+, 'Medical Sch./NUCATS_RSP'
+, 'RSP'
+, 'Active'
+, 61823
+, '192.168.1.221'
+,  sysdate
+)
+
+--- CRU
+--- Clinical Research Unit
+
+INSERT INTO T_INSTITUTIONAL_AFFILIATES
+(
+  AFFILIATE_ID
+, PARENT_ID
+, INSTITUTION_ID
+, PRU_ID
+, PRU_PARENT_ID
+, PRU_CAMPUS
+, NAME_FULL
+, NAME_SORT
+, NAME_ABBREV
+, PRU_STATUS
+, ENTERED_ID
+, ENTERED_IP
+, ENTERED_DATE
+)
+VALUES
+(
+  5005
+, 5003
+, 1008
+, 5005
+, 5003
+, 'Chicago'
+, 'Clinical Research Unit'
+, 'Medical Sch./NUCATS_CRU'
+, 'CRU'
+, 'Active'
+, 61823
+, '192.168.1.221'
+,  sysdate
+)
+
+--- CTI
+--- Center for Translational Innovation
+
+INSERT INTO T_INSTITUTIONAL_AFFILIATES
+(
+  AFFILIATE_ID
+, PARENT_ID
+, INSTITUTION_ID
+, PRU_ID
+, PRU_PARENT_ID
+, PRU_CAMPUS
+, NAME_FULL
+, NAME_SORT
+, NAME_ABBREV
+, PRU_STATUS
+, ENTERED_ID
+, ENTERED_IP
+, ENTERED_DATE
+)
+VALUES
+(
+  5006
+, 555
+, 1008
+, 5006
+, 555
+, 'Chicago'
+, 'Center for Translational Innovation'
+, 'Medical Sch./NUCATS_CTI'
+, 'CTI'
+, 'Active'
+, 61823
+, '192.168.1.221'
+,  sysdate
+)
+
+--- CERC
+--- Community Engaged Research Center
+
+INSERT INTO T_INSTITUTIONAL_AFFILIATES
+(
+  AFFILIATE_ID
+, PARENT_ID
+, INSTITUTION_ID
+, PRU_ID
+, PRU_PARENT_ID
+, PRU_CAMPUS
+, NAME_FULL
+, NAME_SORT
+, NAME_ABBREV
+, PRU_STATUS
+, ENTERED_ID
+, ENTERED_IP
+, ENTERED_DATE
+)
+VALUES
+(
+  5007
+, 555
+, 1008
+, 5007
+, 555
+, 'Chicago'
+, 'Community Engaged Research Center'
+, 'Medical Sch./NUCATS_CERC'
+, 'CERC'
+, 'Active'
+, 61823
+, '192.168.1.221'
+,  sysdate
+)
+
+--- RTC
+--- Research Team Support Program
+
+INSERT INTO T_INSTITUTIONAL_AFFILIATES
+(
+  AFFILIATE_ID
+, PARENT_ID
+, INSTITUTION_ID
+, PRU_ID
+, PRU_PARENT_ID
+, PRU_CAMPUS
+, NAME_FULL
+, NAME_SORT
+, NAME_ABBREV
+, PRU_STATUS
+, ENTERED_ID
+, ENTERED_IP
+, ENTERED_DATE
+)
+VALUES
+(
+  5008
+, 555
+, 1008
+, 5008
+, 555
+, 'Chicago'
+, 'Research Team Support Program'
+, 'Medical Sch./NUCATS_RTC'
+, 'RTC'
+, 'Active'
+, 61823
+, '192.168.1.221'
+,  sysdate
+)
+
+
+--- T & E
+--- Tracking and Evaluation Program
+
+INSERT INTO T_INSTITUTIONAL_AFFILIATES
+(
+  AFFILIATE_ID
+, PARENT_ID
+, INSTITUTION_ID
+, PRU_ID
+, PRU_PARENT_ID
+, PRU_CAMPUS
+, NAME_FULL
+, NAME_SORT
+, NAME_ABBREV
+, PRU_STATUS
+, ENTERED_ID
+, ENTERED_IP
+, ENTERED_DATE
+)
+VALUES
+(
+  5009
+, 555
+, 1008
+, 5009
+, 555
+, 'Chicago'
+, 'Tracking and Evaluation Program'
+, 'Medical Sch./NUCATS_T and E'
+, 'T and E'
 , 'Active'
 , 61823
 , '192.168.1.221'
--- db/user-setup/user-setup.sql
+++ db/user-setup/user-setup.sql
@@ -1,139 +1,110 @@
 
+SELECT * FROM T_SECURITY_LOGINS WHERE PORTAL = 'ClearCATS'
+SELECT * FROM T_SECURITY_GROUP_MEMBERS WHERE PORTAL = 'ClearCATS'
+
 -- add logins for portal
 
 INSERT INTO T_SECURITY_LOGINS
-(
-  USERNAME
-, PORTAL
-, CHALLENGE
-, EXPIRE_ON
-, ENTERED_BY
-, ENTERED_IP
-, ENTERED_DATE
-)
+( USERNAME, PORTAL, CHALLENGE, EXPIRE_ON, ENTERED_BY, ENTERED_IP, ENTERED_DATE )
 VALUES
-(
-  'pfr957'
-, 'ClearCATS'
-, null
-, null
-, 'pfr957'
-, '165.124.223.127'
-, sysdate
-)
+( 'pfr957', 'ClearCATS', null, null, 'pfr957', '165.124.223.127', sysdate )
 
--- skk958, aco454
 
 INSERT INTO T_SECURITY_LOGINS
-(
-  USERNAME
-, PORTAL
-, CHALLENGE
-, EXPIRE_ON
-, ENTERED_BY
-, ENTERED_IP
-, ENTERED_DATE
-)
+( USERNAME, PORTAL, CHALLENGE, EXPIRE_ON, ENTERED_BY, ENTERED_IP, ENTERED_DATE )
 VALUES
-(
-  'mns521'
-, 'ClearCATS'
-, null
-, null
-, 'pfr957'
-, '165.124.223.127'
-, sysdate
-)
+( 'mns521', 'ClearCATS', null, null, 'pfr957', '165.124.223.127', sysdate )
+
 
 INSERT INTO T_SECURITY_LOGINS
-(
-  USERNAME
-, PORTAL
-, CHALLENGE
-, EXPIRE_ON
-, ENTERED_BY
-, ENTERED_IP
-, ENTERED_DATE
-)
+( USERNAME, PORTAL, CHALLENGE, EXPIRE_ON, ENTERED_BY, ENTERED_IP, ENTERED_DATE )
 VALUES
-(
-  'cmix'
-, 'ClearCATS'
-, null
-, null
-, 'pfr957'
-, '165.124.223.127'
-, sysdate
-)
+( 'cmix', 'ClearCATS', null, null, 'pfr957', '165.124.223.127', sysdate )
 
+
+INSERT INTO T_SECURITY_LOGINS
+( USERNAME, PORTAL, CHALLENGE, EXPIRE_ON, ENTERED_BY, ENTERED_IP, ENTERED_DATE )
+VALUES
+( 'wakibbe', 'ClearCATS', null, null, 'pfr957', '165.124.223.127', sysdate )
+
+
+INSERT INTO T_SECURITY_LOGINS
+( USERNAME, PORTAL, CHALLENGE, EXPIRE_ON, ENTERED_BY, ENTERED_IP, ENTERED_DATE )
+VALUES
+( 'aco454', 'ClearCATS', null, null, 'pfr957', '165.124.223.127', sysdate )
+
+
+INSERT INTO T_SECURITY_LOGINS
+( USERNAME, PORTAL, CHALLENGE, EXPIRE_ON, ENTERED_BY, ENTERED_IP, ENTERED_DATE )
+VALUES
+( 'skk958', 'ClearCATS', null, null, 'pfr957', '165.124.223.127', sysdate )
+
+---
 -- assign personnel to groups -- ensure affiliate_id matches that in institutional affiliates
+---
 
 INSERT INTO T_SECURITY_GROUP_MEMBERS
-(
-  SGM_ID
-, USERNAME
-, GROUP_NAME
-, AFFILIATE_ID
-, PORTAL
-, entered_by
-, entered_ip
-, entered_date
-)
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
 VALUES
-(
-  SGM_ID_SEQ.nextval
-, 'pfr957'
-, 'Admin'
-, 555 --- (SELECT AFFILIATE_ID FROM T_INSTITUTIONAL_AFFILIATES WHERE NAME_ABBREV = 'NUCATS') 
-, 'ClearCATS'
-, 'pfr957'
-, '165.124.223.127'
-,  sysdate
-);
-
-INSERT INTO T_SECURITY_GROUP_MEMBERS
-(
-  SGM_ID
-, USERNAME
-, GROUP_NAME
-, AFFILIATE_ID
-, PORTAL
-, entered_by
-, entered_ip
-, entered_date
-)
-VALUES
-(
-  SGM_ID_SEQ.nextval
-, 'cmix'
-, 'Admin'
-, 555 
-, 'ClearCATS'
-, 'pfr957'
-, '165.124.223.127'
-,  sysdate
-);
+( SGM_ID_SEQ.nextval, 'pfr957', 'Admin', 555, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
 
 
 INSERT INTO T_SECURITY_GROUP_MEMBERS
-(
-  SGM_ID
-, USERNAME
-, GROUP_NAME
-, AFFILIATE_ID
-, PORTAL
-, entered_by
-, entered_ip
-, entered_date
-)
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
 VALUES
-(
-  SGM_ID_SEQ.nextval
-, 'mns521'
-, 'Admin'
-, 5002
-, 'ClearCATS'
-, 'pfr957'
-, '165.124.223.127'
-,  sysdate
-);
+( SGM_ID_SEQ.nextval, 'cmix', 'Admin', 555, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
+
+
+INSERT INTO T_SECURITY_GROUP_MEMBERS
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
+VALUES
+( SGM_ID_SEQ.nextval, 'mns521', 'Admin', 5002, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
+
+
+INSERT INTO T_SECURITY_GROUP_MEMBERS
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
+VALUES
+( SGM_ID_SEQ.nextval, 'wakibbe', 'Admin', 555, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
+
+
+INSERT INTO T_SECURITY_GROUP_MEMBERS
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
+VALUES
+( SGM_ID_SEQ.nextval, 'aco454', 'Admin', 555, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
+
+-- 10/25
+
+INSERT INTO T_SECURITY_GROUP_MEMBERS
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
+VALUES
+( SGM_ID_SEQ.nextval, 'jab155', 'Admin', 5006, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
+
+
+INSERT INTO T_SECURITY_GROUP_MEMBERS
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
+VALUES
+( SGM_ID_SEQ.nextval, 'hjf799', 'Admin', 5008, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
+
+
+INSERT INTO T_SECURITY_GROUP_MEMBERS
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
+VALUES
+( SGM_ID_SEQ.nextval, 'ltr435', 'Admin', 5008, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
+
+
+INSERT INTO T_SECURITY_GROUP_MEMBERS
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
+VALUES
+( SGM_ID_SEQ.nextval, 'bfs326', 'Admin', 5004, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
+
+
+INSERT INTO T_SECURITY_GROUP_MEMBERS
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
+VALUES
+( SGM_ID_SEQ.nextval, 'tha209', 'Admin', 5004, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
+
+
+INSERT INTO T_SECURITY_GROUP_MEMBERS
+( SGM_ID, USERNAME, GROUP_NAME, AFFILIATE_ID, PORTAL, entered_by, entered_ip, entered_date )
+VALUES
+( SGM_ID_SEQ.nextval, 'pcm777', 'Admin', 5005, 'ClearCATS', 'pfr957', '165.124.223.127', sysdate );
