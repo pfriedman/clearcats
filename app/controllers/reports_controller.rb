f9c0e34721f0f565200ede698019ddd5bf5201e0
Paul Friedman <p-friedman@northwestern.edu>
2010-10-27 16:47:46 -0500

    export results services menu item goes to index

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,6 +9,10 @@ class ReportsController < ApplicationController
     @user_organizational_units = determine_org_units_for_user
     @search = Client.search(params[:search])
     @clients = @search.paginate(:page => params[:page], :per_page => 20)
+    respond_to do |format|
+      format.html # index.html.erb
+      format.csv { render :csv => @search.all }
+    end
   end
 
 end
