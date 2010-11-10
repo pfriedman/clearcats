1b10e17b602b0e0be3df41ebf8c37f0c5fb53a48
Paul Friedman <p-friedman@northwestern.edu>
2010-11-10 16:56:35 -0600

    don't validate email

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -168,7 +168,7 @@ describe Person do
   
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:first_name) }
-  it { should validate_presence_of(:email) }
+  # it { should validate_presence_of(:email) }
   
   describe "a person who does not have a netid" do 
     before(:each) do
